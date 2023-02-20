e7f5e1d5965469559f9e209a4c32e5bc1dc35058
champwow <51086340+champwow@users.noreply.github.com>
2023-02-21 02:31:24 +0700

    เพิ่ม about me

--- Script.js
+++ Script.js
@@ -14,33 +14,58 @@ function currentSlide(n) {
 
 
 function reveal() {
-    
+
     var reveals = document.querySelectorAll(".reveal");
+    var revealsAboutMe = document.querySelectorAll(".reveal-aboutMe");
     var textMywork = document.getElementById("mywork");
+    var textAboutMe = document.getElementById("AboutMe");
 
-    // var Intro = document.querySelector("hero");
-    // var textIntro = document.getElementById("Intro");
-    // var IntroelementTop = Intro.getBoundingClientRect().top;
-    // if (IntroelementTop < windowHeight - 150){
-    //     textIntro.style.color = "#30CFD0";
-    //     textIntro.style.fontSize = "1.5vw"
-    // }
+    for (let i = 0; i < revealsAboutMe.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = revealsAboutMe[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible && elementTop > -200) {
+            revealsAboutMe[i].classList.add("REactive");
+            textAboutMe.style.color = "#30CFD0";
+            textAboutMe.style.fontSize = "1.5vw"
 
+
+        }
+        else {
+            revealsAboutMe[i].classList.remove("REactive");
+            textAboutMe.style.color = "#ffffff";
+            textAboutMe.style.fontSize = "1vw"
+        }
+        //  else {
+        //     revealsAboutMe[i].classList.remove("REactive");
+        //     textAboutMe.style.color = "#ffffff";
+        //     textAboutMe.style.fontSize = "1vw"
+        // }
+
+    }
 
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("REactive");
+        var elementTopHead = document.getElementById("revalHead").getBoundingClientRect().top;;
+        var elementVisible = 200;
+        console.log(elementTopHead);
+        if (elementTopHead < windowHeight - elementVisible && elementTopHead > -500) {
             textMywork.style.color = "#30CFD0";
             textMywork.style.fontSize = "1.5vw"
+        }
+        else{
+            textMywork.style.color = "#ffffff";
+            textMywork.style.fontSize = "1vw"
+        }
+        if (elementTop < windowHeight - elementVisible && elementTop > -500) {
+            reveals[i].classList.add("REactive");
+
+
 
         } else {
             reveals[i].classList.remove("REactive");
-            textMywork.style.color = "#ffffff";
-            textMywork.style.fontSize = "1vw"
+           
         }
     }
 }
@@ -65,12 +90,16 @@ function showSlides(n) {
 function showmenu() {
     console.log("sdsdsdsdsds");
     let myworktext = document.getElementById("mywork");
-    myworktext.innerHTML = "MY WORK"
+    let AboutMetext = document.getElementById("AboutMe");
+    myworktext.innerHTML = "MY WORK";
+    AboutMetext.innerHTML = "ABOUT ME";
 }
 
 function cancleShowMenu() {
     let myworktext = document.getElementById("mywork");
+    let AboutMetext = document.getElementById("AboutMe");
     myworktext.innerHTML = "."
+    AboutMetext.innerHTML = ".";
 }
 
 
